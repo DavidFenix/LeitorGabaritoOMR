55e1b07976a09aa7ec8d925d0ec0366734aa2577
David Costa <deividesantoscosta@gmail.com>
2026-08-14 09:55:40 -0300

    Remove arquivos gerados do build do OpenCV

--- opencv/build/intermediates/cxx/Debug/372l2z4m/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\PCLEI14\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\PCLEI14\\AndroidStudioProjects\\LeitorGabaritoOMR\\opencv\\.cxx\\Debug\\372l2z4m\\x86_64" ^
-  opencv_jni_shared
